503fc4ddf1162f82b71b958207be01b03c01ea08
HejHTML <emmasmejladress@gmail.com>
2025-11-15 13:24:46 +0100

    starting project added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import data from "./data.json"
+import { Hero } from "./components/Hero";
+import { Skills } from "./components/Skills";
+import { Divider } from "./components/Divider";
+import { FeaturedProjects } from "./components/FeaturedProjects";
+import { ProjectsSection } from "./components/ProjectsSection";
+import { Contact } from "./components/Contact";
+import data from "./data.json";
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>
-}
+  return (
+    <main>
+      <Hero />
+      <Skills />
+      <Divider />
+      {/* Visa de 4 första projekten som Featured */}
+      <FeaturedProjects projects={data.projects.slice(0, 4)} />
+      {/* Visa alla projekt */}
+      <ProjectsSection projects={data.projects} />
+      <Contact />
+    </main>
+  );
+};
