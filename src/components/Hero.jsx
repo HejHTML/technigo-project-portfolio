695819f887bb68494aef821a642e8a0916170cb9
HejHTML <emmasmejladress@gmail.com>
2025-11-16 18:38:21 +0100

    hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,12 @@ export const Hero = () => {
         <section className="hero">
 
             <h1>Emma Franzén</h1>
-            <p>Frontend Developer based in Sweden</p>
+            <p className="work-title">Frontend Developer based in Sweden</p>
             <p className="hero-intro">
                 Hej, jag heter Emma och jag pluggar till webbutvecklare på fritiden. Just nu kodar jag den här sidan som ska bli min portfolio.
             </p>
             <img
-                src="src/assets/emma.jpg"
+                src="src/assets/emma4.jpg"
                 alt="Profile"
                 className="hero-img"
             />
