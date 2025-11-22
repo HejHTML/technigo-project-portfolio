fcaa7937960811b9dcb7c52a03dba2b6bbfc7222
HejHTML <emmasmejladress@gmail.com>
2025-11-22 11:52:58 +0100

    projectcardjsx img

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,9 @@
 export const ProjectCard = ({ project }) => {
+    const imageUrl = new URL(`../assets/${project.image}`, import.meta.url).href;
+
     return (
         <div className="project-card">
-            <img src={project.image} alt={project.name} className="project-image" />
+            <img src={imageUrl} alt={project.name} className="project-image" />
 
             <h3 className="project-title">{project.name}</h3>
 
@@ -12,10 +14,10 @@ export const ProjectCard = ({ project }) => {
             </div>
 
             <div className="project-buttons">
-                <a href={project.netlify} className="btn btn-live" target="_blank">
+                <a href={project.netlify} className="btn btn-live" target="_blank" rel="noopener noreferrer">
                     Live demo
                 </a>
-                <a href={project.github} className="btn btn-code" target="_blank">
+                <a href={project.github} className="btn btn-code" target="_blank" rel="noopener noreferrer">
                     View code
                 </a>
             </div>
