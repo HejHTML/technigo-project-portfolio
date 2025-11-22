12c76da0ce1ebbfcfcc18d36ee676ec973ea0628
HejHTML <emmasmejladress@gmail.com>
2025-11-22 11:55:32 +0100

    profileimg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,19 @@
 export const Hero = () => {
+    const heroImage = new URL("../assets/emma4.jpg", import.meta.url).href;
+
     return (
         <section className="hero">
-
             <h1>Emma Franzén</h1>
             <p className="work-title">Frontend Developer based in Sweden</p>
             <p className="hero-intro">
                 Hej, jag heter Emma och jag pluggar till webbutvecklare på fritiden. Just nu kodar jag den här sidan som ska bli min portfolio.
             </p>
+
             <img
-                src="src/assets/emma4.jpg"
+                src={heroImage}
                 alt="Profile"
                 className="hero-img"
             />
-
-            <div className="hero-buttons">
-
-            </div>
         </section>
     );
 };
