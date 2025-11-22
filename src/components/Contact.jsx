e4738179041017aade6e566e8dc11236b3f61991
HejHTML <emmasmejladress@gmail.com>
2025-11-22 17:03:36 +0100

    added socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,19 @@
+import React from "react";
+import LinkedInLogo from "../assets/linkedin.svg";
+import "../index.css";
+
 export const Contact = () => {
     return (
         <section className="contact">
-            <h2>Let's talk</h2>
+            <h2>Let's talk!</h2>
             <p>Email: emmasmejladress@gmail.com</p>
-            <p>
-                LinkedIn:{" "}
+            <p className="linkedIn">
                 <a
                     href="https://www.linkedin.com/in/emma-franz%C3%A9n-90874346/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
+                    <img src={LinkedInLogo} alt="LinkedIn" />
                     emma.franzen
                 </a>
             </p>
