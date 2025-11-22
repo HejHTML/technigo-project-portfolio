d17477a18a669fa90e472b7eb58eb4da553bc12f
HejHTML <emmasmejladress@gmail.com>
2025-11-22 12:48:56 +0100

    divider 2 skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,27 @@
+import { Divider } from "./Divider";
+
 export const Skills = () => {
     return (
         <section className="skills">
-            <h2>Skills</h2>
+            <h2>My Skills</h2>
+
+            {/* First list */}
             <ul>
-                <li>HTML</li>
-                <li>CSS</li>
+                <li>HTML5</li>
+                <li>CSS3</li>
                 <li>JavaScript</li>
                 <li>React</li>
                 <li>APIs</li>
+            </ul>
+
+            {/* Second list */}
+            <Divider />
+
+            {/* Andra listan */}
+            <ul>
+                <li>React</li>
+                <li>Git</li>
+                <li>Node.js</li>
             </ul>
         </section>
     );
