eeb521b35b5f02b0e3182e223fd2debc59faed3b
HejHTML <emmasmejladress@gmail.com>
2025-11-23 14:17:34 +0100

    deleted email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export const Contact = () => {
     return (
         <section className="contact">
             <h2>Let's talk!</h2>
-            <p>Email: emmasmejladress@gmail.com</p>
+
             <p className="linkedIn">
                 <a
                     href="https://www.linkedin.com/in/emma-franz%C3%A9n-90874346/"
