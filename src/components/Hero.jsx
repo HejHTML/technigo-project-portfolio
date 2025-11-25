cafbe84dd611a38e9281e1e5b03259d9e22eab4f
HejHTML <emmasmejladress@gmail.com>
2025-11-25 17:34:53 +0100

    added background about me

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,15 +8,17 @@ export const Hero = () => {
                 <p className="work-title">Frontend Developer</p>
 
                 <p className="hero-intro">
-                    Hi, my name is Emma, and I’m studying web development in my free time. Currently, I’m building this website as my personal portfolio to showcase the projects I’ve been working on.
+                    Hi, my name is Emma, and I am studying web development. Currently, I am building this website as my personal portfolio to showcase the projects I have been working on.
+                </p>
+                <p className="hero-intro">I am a trained behavioral scientist and folk high school teacher with a focus on public health and pedagogy and I love learning new things, whether it's lindy hop, playing the piano or learning to code. My motto is: ‘If you don’t do anything, nothing will happen.’
                 </p>
 
                 <p className="hero-intro">
-                    On this site, you’ll find a variety of projects I’ve created, ranging from responsive websites built with HTML and CSS to interactive applications using JavaScript and APIs.
+                    On this site, you’ll find a variety of projects I have created, ranging from responsive websites built with HTML and CSS to interactive applications using JavaScript and APIs.
                 </p>
 
                 <p className="hero-intro">
-                    I hope this portfolio gives you an idea of my work, my approach to web development, and the progress I’ve made so far. Feel free to explore the projects, check out the live demos, and view the code on GitHub for a deeper look at how I built them.
+                    I hope this portfolio gives you an idea of my work, my approach to web development, and the progress I have made so far. Feel free to explore the projects, check out the live demos, and view the code on GitHub for a deeper look at how I built them.
                 </p>
             </div>
 
