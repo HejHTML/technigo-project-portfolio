13e09175bd43217da22e69f7288504738898d016
HejHTML <emmasmejladress@gmail.com>
2026-02-28 14:01:43 +0100

    added new hero correct

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
                 <p className="hero-intro">
                     Tjabba! Emma här. Jag studerar webbutveckling hos Technigo sedan hösten 2025. Den här webbplatsen är min portfolio där jag visar upp de projekt jag har arbetat med under kursen.
                 </p>
-                <p className="hero-intro">Tidigare har jag arbetat som folkhögskollärare, mentor och hälsopedagog. Jag kan lite om mycket och har aldrig slutat plugga nya saker under tiden som jag jobbat. Dialekter och ortnamn, religionsfilosofi, brukspiano och musikteater är några små stickspår som givit mig nya insikter. Lindy hop och improvisationsteater har följt med mig länge.
+                <p className="hero-intro">Tidigare har jag arbetat som folkhögskollärare, mentor och hälsopedagog. Jag kan lite om mycket och har aldrig slutat plugga nya saker under tiden jag jobbat. Dialekter och ortnamn, religionsfilosofi, brukspiano och musikteater är några små stickspår som givit mig nya insikter. Lindy hop och improvisationsteater har följt med länge.
                 </p>
 
                 <p className="hero-intro">
